60cdf1626e1ac26d36d84db0d2eb311b3df58678
Laurence Moughan <123552014+eilaurencemoughan@users.noreply.github.com>
2023-04-28 15:43:49 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -54,16 +54,18 @@ module "blog_sg" {
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
 resource "aws_route_table" "example" {
-  vpc_id = aws_vpc.example.id
+  vpc_id = module.blog_vpc.vpc_id
 
   route {
     cidr_block = "10.0.1.0/24"
-    gateway_id = aws_internet_gateway.example.id
+    
   }
 
   route {
     ipv6_cidr_block        = "::/0"
-    egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
+    
+    
+
   }
 
   tags = {
